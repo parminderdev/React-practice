fc8484c76207b4c1b450de7b163da4fe805499a8
parminderdev <parminder.singh.coder@gmail.com>
2021-06-26 10:07:34 +1200

    Created New Day1 Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Day1 from "./components/Day1";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   Container,
@@ -10,97 +11,14 @@ import {
 } from "react-bootstrap";
 function App() {
   return (
-    <Container className="jumbotron text-center bg-dark text-white">
-      <Row>
-        <h2>React Practice Step By Step By Parminder</h2>
-      </Row>
-      <Row>
-        <h4>React Bootstrap Package Practice Day 1</h4>
-      </Row>
-      <Row className="bg-light">
-        <Nav defaultActiveKey="/home" as="ul">
-          <Nav.Item as="li">
-            <Nav.Link href="/home">Active</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Row>
-      <Row className="m-5">
-        <Col className="text-left">
-          <Form>
-            <Form.Group>
-              <Form.Label>Enter your full name:</Form.Label>
-              <Form.Control type="text" placeholder="Enter your full name" />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label className="text-left">
-                Enter your email address:
-              </Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter your your email address"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Enter your age:</Form.Label>
-              <Form.Control type="number" placeholder="Enter your age" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Click here to submit form
-            </Button>
-          </Form>
-        </Col>
-        <Col>
-          <Carousel fade>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://picsum.photos/id/237/400/270"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://picsum.photos/id/238/400/270"
-                alt="Second slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://picsum.photos/id/239/400/270"
-                alt="Third slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </Col>
-      </Row>
-    </Container>
+    // Use Day 1 Component <Day1></Day1>
+    <div>
+      <Container className="jumbotron text-center bg-dark text-white">
+        <Row>
+          <h2>React Practice Step By Step By Parminder</h2>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
