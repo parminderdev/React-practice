094b33fa970c00bb45c8e2d9f1ca284bf7dac8c5
parminderdev <parminder.singh.coder@gmail.com>
2021-06-25 17:37:47 +1200

    React Bootstrap Practice
    I added a package of react-bootstrap.
    I created a page design with container, row , button, col, form, carousel and nav components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,107 @@
-import './App.css';
-import Ad from './components/abcApp';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  Row,
+  Button,
+  Col,
+  Form,
+  Carousel,
+  Nav,
+} from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <h1>This is my new app</h1>
-      <Ad></Ad>
-    </div>
+    <Container className="jumbotron text-center bg-dark text-white">
+      <Row>
+        <h2>React Practice Step By Step By Parminder</h2>
+      </Row>
+      <Row>
+        <h4>Bootstrap Package Practice</h4>
+      </Row>
+      <Row className="bg-light">
+        <Nav defaultActiveKey="/home" as="ul">
+          <Nav.Item as="li">
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Row>
+      <Row className="m-5">
+        <Col className="text-left">
+          <Form>
+            <Form.Group>
+              <Form.Label>Enter your full name:</Form.Label>
+              <Form.Control type="text" placeholder="Enter your full name" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label className="text-left">
+                Enter your email address:
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your your email address"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Enter your age:</Form.Label>
+              <Form.Control type="number" placeholder="Enter your age" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Click here to submit form
+            </Button>
+          </Form>
+        </Col>
+        <Col>
+          <Carousel fade>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/id/237/400/270"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/id/238/400/270"
+                alt="Second slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/id/239/400/270"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
-
 
 export default App;
