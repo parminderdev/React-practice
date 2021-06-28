b1053054f6a016d4a6e530e4071c89e43f0c86c6
parminderdev <parminder.singh.coder@gmail.com>
2021-06-28 13:06:46 +1200

    Calling onClick function for update states

--- src/components/Day3.jsx
+++ src/components/Day3.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 export default class Day3 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Name: "Parminder",
+      Email: "parmmehra@gmail.com",
+      Edu: "B.TECH CSE",
+    };
+  }
+  function updateDetail() {
+    this.setState({ Name: "Demo", Email: "demo@demo.com", Edu: "CSE" });
+  }
   render() {
-    return <div>THis is Day3 Component</div>;
+    return (
+      <div>
+        <h3 style={{ textDecoration: "underline" }}>Day3 Component</h3>
+        <h4>User Detail</h4>
+        <p>
+          <span>
+            Name:<input type="text" value={this.state.Name}></input>
+            <br></br>
+          </span>
+          <span>
+            Email: <input type="text" value={this.state.Email}></input>
+            <br></br>
+          </span>
+          <span>
+            Education: <input type="text" value={this.state.Edu}></input>
+            <br></br>
+          </span>
+          <span>
+            <button onClick={() => updateDetail()}>Update</button>
+          </span>
+        </p>
+      </div>
+    );
   }
 }
