0bb6bdb2b85756b6547caf7f7af28a8cc3f8bdc5
parminderdev <parminder.singh.coder@gmail.com>
2021-06-28 12:56:25 +1200

    Converted Day3 function component to class component

--- src/components/Day3.jsx
+++ src/components/Day3.jsx
@@ -1,9 +1,6 @@
-function Day3() {
-  return (
-    <div>
-      <h3>Day3 page </h3>
-    </div>
-  );
+import React from "react";
+export default class Day3 extends React.Component {
+  render() {
+    return <div>THis is Day3 Component</div>;
+  }
 }
-
-export default Day3;
