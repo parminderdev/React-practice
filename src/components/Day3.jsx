6269db1b28130893fe7bd7b599e84e38907d518c
parminderdev <parminder.singh.coder@gmail.com>
2021-07-02 00:40:47 +1200

    Form handing with controlled and uncontrolled inputs

--- src/components/Day3.jsx
+++ src/components/Day3.jsx
@@ -12,6 +12,9 @@ export default class Day3 extends React.Component {
   render() {
     return (
       <div>
+        <div style={{ backgroundColor: "grey", margin: 20, padding: 20 }}>
+          <h3>Form Handling with class component</h3>
+        </div>
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
@@ -19,7 +22,44 @@ export default class Day3 extends React.Component {
           </label>
           <input type="submit" name="Submit" />
         </form>
+        <div style={{ backgroundColor: "grey", margin: 20, padding: 20 }}>
+          <h3>Form Handling with function component</h3>
+        </div>
+        <LoginForm></LoginForm>
       </div>
     );
   }
+}
+
+function LoginForm(props) {
+  const nameEl = React.useRef(null);
+  const passwordEl = React.useRef(null);
+  const rememberMeEl = React.useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: nameEl.current.value,
+      password: passwordEl.current.value,
+      rememberMe: rememberMeEl.current.checked,
+    };
+
+    // Submit form details to login endpoint etc.
+    // ...
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="username" ref={nameEl} />
+      <input type="password" placeholder="password" ref={passwordEl} />
+      <label>
+        <input type="checkbox" ref={rememberMeEl} />
+        Remember me
+      </label>
+      <button type="submit" className="myButton">
+        Login
+      </button>
+    </form>
+  );
 }
