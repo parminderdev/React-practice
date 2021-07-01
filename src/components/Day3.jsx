7aea32c493b7c517c96ffd077ec1428dfe64f3bd
parminderdev <parminder.singh.coder@gmail.com>
2021-07-02 00:24:31 +1200

    Trying Uncontrolled Inputs using class components

--- src/components/Day3.jsx
+++ src/components/Day3.jsx
@@ -1,49 +1,24 @@
 import React from "react";
 export default class Day3 extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      Name: "Parminder",
-      Email: "parmmehra@gmail.com",
-      Edu: "B.TECH CSE",
-    };
+  constructor(props) {
+    super(props);
+    this.nameEL = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  updateDetail = () => {
-    let idanme = document.querySelector("#name").value;
-    let idemail = document.querySelector("#email").value;
-    let idedv = document.querySelector("#edu").value;
-    this.setState({ Name: idanme, Email: idemail, Edu: idedv });
-  };
+  handleSubmit(e) {
+    e.preventDefault();
+    alert(this.nameEl.current.value);
+  }
   render() {
     return (
       <div>
-        <h3 style={{ textDecoration: "underline" }}>Day3 Component</h3>
-        <h4>User Detail</h4>
-        <p>
-          <span>
+        <form onSubmit={this.handleSubmit}>
+          <label>
             Name:
-            <input
-              id="name"
-              type="text"
-              onChange={this.updateDetail}
-              value=""
-            ></input>
-            <br></br>
-          </span>
-          <span>
-            Email:
-            <input id="email" type="text" value=""></input>
-            <br></br>
-          </span>
-          <span>
-            Education:
-            <input id="edu" type="text" value=""></input>
-            <br></br>
-          </span>
-          <span>
-            <button onClick={this.updateDetail}>Update</button>
-          </span>
-        </p>
+            <input type="text" ref={this.nameEl} />
+          </label>
+          <input type="submit" name="Submit" />
+        </form>
       </div>
     );
   }
