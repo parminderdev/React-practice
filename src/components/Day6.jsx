7cb071f032e2984be946ad861305ec32087f6e4f
parminderdev <parminder.singh.coder@gmail.com>
2021-07-25 13:18:01 +1200

    get data from input feild in functional component

--- src/components/Day6.jsx
+++ src/components/Day6.jsx
@@ -1,12 +1,18 @@
 import Sidebar from "./Sidebar";
 import { Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
+
+// Functional Component for get data from input feild and print on click button
 export default function Day6() {
   const [data, setData] = useState(null);
+  const [print1, setPrint] = useState(false);
+
   function getdata(val) {
     console.log(val.target.value);
     setData(val.target.value);
+    setPrint(false);
   }
+
   return (
     <Container>
       <Row>
@@ -18,8 +24,14 @@ export default function Day6() {
             <h1>Day6 Practice page</h1>
             <p>Get input from Inbox</p>
             <p>Input Value is : {data}</p>
+            {print1 ? data : "Button not clicked yet"}
             <br></br>
             <input type="text" onChange={getdata}></input>
+            <input
+              type="button"
+              value="Print Data"
+              onClick={() => setPrint(true)}
+            ></input>
           </div>
         </Col>
       </Row>
