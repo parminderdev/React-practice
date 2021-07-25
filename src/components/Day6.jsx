454779efcdbeab948a4c6bb12874fdccb59b126c
parminderdev <parminder.singh.coder@gmail.com>
2021-07-25 13:06:37 +1200

    Get Data From Input Field Functional component

--- src/components/Day6.jsx
+++ src/components/Day6.jsx
@@ -1,6 +1,12 @@
 import Sidebar from "./Sidebar";
 import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
 export default function Day6() {
+  const [data, setData] = useState(null);
+  function getdata(val) {
+    console.log(val.target.value);
+    setData(val.target.value);
+  }
   return (
     <Container>
       <Row>
@@ -10,6 +16,10 @@ export default function Day6() {
         <Col xs={9}>
           <div>
             <h1>Day6 Practice page</h1>
+            <p>Get input from Inbox</p>
+            <p>Input Value is : {data}</p>
+            <br></br>
+            <input type="text" onChange={getdata}></input>
           </div>
         </Col>
       </Row>
